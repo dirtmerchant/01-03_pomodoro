6ef9599cf1059ed8ef591d9a641f620b9c97eff2
Bert Bullough <bert@bertbullough.com>
2018-07-03 14:21:46 -0600

    defined current time method

--- program.py
+++ program.py
@@ -1,10 +1,23 @@
-def header:
-    pass
+from datetime import datetime, timedelta
 
 
-def current_time:
-    pass
+def main():
+    print_header()
+    current_time()
 
 
-def twenty_five_minutes:
-    pass
+def print_header():
+    print('-----------------------')
+    print('    Pomodoro Timer')
+    print('-----------------------')
+
+
+def current_time():
+    now = datetime.today()
+    print('The time is {}'.format(now))
+    now_plus_25m = now + timedelta(minutes=25)
+    print('Twenty-five minutes from now will be {}'.format(now_plus_25m))
+
+
+if __name__ == '__main__':
+    main()
