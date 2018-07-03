c2ff3217a48fce5e2c2e57bd32269e2e9ecbf1bb
Bert Bullough <bert@bertbullough.com>
2018-07-03 15:26:08 -0600

    sleep timer working

--- program.py
+++ program.py
@@ -1,9 +1,10 @@
-from datetime import datetime, timedelta
-
+import time as t
+import datetime
 
 def main():
     print_header()
-    current_time()
+    # current_time()
+    start_pomodoro_timer()
 
 
 def print_header():
@@ -12,11 +13,25 @@ def print_header():
     print('-----------------------')
 
 
-def current_time():
-    now = datetime.today()
-    print('The time is {}'.format(now))
-    now_plus_25m = now + timedelta(minutes=25)
-    print('Twenty-five minutes from now will be {}'.format(now_plus_25m))
+# def current_time():
+#     now = datetime.today()
+#     print('The time is {}'.format(now))
+#     now_plus_25m = now + timedelta(minutes=25)
+#     print('Twenty-five minutes from now will be {}'.format(now_plus_25m))
+
+
+def start_pomodoro_timer():
+    while True:
+        cmd = input('Start a new [p]omodoro task, [b]reak, or e[x]it?')
+        if cmd == 'p':
+            print('Time to work')
+            t.sleep(25)
+        elif cmd == 'b':
+            print('Time for a break')
+            t.sleep(5)
+        elif cmd == 'x':
+            print('Exiting')
+            break
 
 
 if __name__ == '__main__':
